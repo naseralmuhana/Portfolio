944a1bc5f922b6bbaa1b2181c664cdf4a9e8f930
naseralmuhana <naseralmuhana0@gmail.com>
2023-03-03 18:56:12 +0300

    update

--- src/pages/Home/components/Header/index.jsx
+++ src/pages/Home/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import useScrollTrigger from "@mui/material/useScrollTrigger"
 import LogoAnimation from "./components/Logo"
-import Menu from "./components/Menu"
+
 import Style from "./style"
 
 const Header = () => {
@@ -8,7 +8,9 @@ const Header = () => {
   return (
     <Style.Wrapper trigger={trigger}>
       <LogoAnimation trigger={trigger} />
-      <Menu />
+      <div className="trigger">
+        <span />
+      </div>
     </Style.Wrapper>
   )
 }
--- src/pages/Home/components/Header/style.js
+++ src/pages/Home/components/Header/style.js
@@ -17,6 +17,51 @@ const Wrapper = styled("header")(({ theme, trigger }) => ({
   top: 0,
   left: 0,
   zIndex: 99,
+
+  [".trigger"]: {
+    display: "block",
+    width: "50px",
+    borderRadius: "100%",
+    color: "#bbb",
+    cursor: "pointer",
+
+    zIndex: 5,
+    position: "relative",
+
+    height: "50px",
+    backgroundColor: "#000",
+    "&::after": {
+      content: '""',
+      position: "absolute",
+      zIndex: -1,
+      width: "100%",
+      height: "100%",
+      top: 0,
+      left: 0,
+      color: "#cc00ff",
+      opacity: 0.15,
+      borderRadius: "100%",
+      boxShadow: "0 5px 20px",
+      transition: "0.3s",
+    },
+
+    span: {
+      top: "24px",
+      width: "24px",
+      left: "13px",
+      "&::before": { bottom: "6px", width: "24px", left: 0 },
+      "&::after": { top: "6px", width: "16px", left: 0 },
+    },
+
+    "span, span::after, span::before": {
+      display: "block",
+      height: "2px",
+      position: "absolute",
+      backgroundColor: "#bbb",
+      content: '""',
+      transition: "0.3s",
+    },
+  },
 }))
 
 export default {
